f1f05b7fc939f175792e0cc19b497936397743f7
Peter Jang <peter.x.jang@gmail.com>
2018-03-07 21:10:38 -0600

    Add solution for guess url segment game

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   namespace :v1 do
     get "/guess_query_url" => "games#guess_query_method"
     get "/real_guess_query_url" => "games#real_guess_query_method"
+    get "/real_guess_url_segment_url/:user_guess" => "games#real_guess_query_method"
   end
 end
